d4411e5e2e155609cefc4b316b9fc7e3af45fd7a
Ben Morse <snowyote@gmail.com>
2017-11-19 17:07:32 -0800

    this appears to work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Slider from 'react-rangeslider';
 import 'react-rangeslider/lib/index.css';
-import Sound from 'react-sound';
+import {soundManager} from 'soundmanager2';
 
 const soundUrls = [
   'http://www.denhaku.com/r_box/ddd1/bass1.wav',
@@ -11,6 +11,15 @@ const soundUrls = [
   'http://www.denhaku.com/r_box/sr16/sr16hat/edge%20hat.wav',
   'http://www.denhaku.com/r_box/sr16/sr16sd/dynohlsn.wav',
 ];
+
+const buttonCols = [0, 1, 2, 3, 4, 5, 6, 7];
+const buttonRows = ['Sound0', 'Sound1', 'Sound2', 'Sound3'];
+const soundFiles = {
+  Sound0: 'http://www.denhaku.com/r_box/ddd1/bass1.wav',
+  Sound1: 'http://www.whitenote.dk/Download%20Frame/Whitenote%20Sampels/Slave%20of%20your%20lust/Hi-hat%203.wav',
+  Sound2: 'http://www.denhaku.com/r_box/sr16/sr16hat/edge%20hat.wav',
+  Sound3: 'http://www.denhaku.com/r_box/sr16/sr16sd/dynohlsn.wav',
+};
 
 class SampleButton extends React.Component {
   render() {
@@ -51,14 +60,12 @@ class Sampler extends React.Component {
     super(props);
     this.state = {
       buttons: Array(4).fill(Array(8).fill(false)),
-      playStatus: Array(4).fill(Array(8).fill('STOPPED')),
       currentBeat: 0,
     };
   }
 
   makeColumnOfButtons(column, name, soundUrl) {
-    const buttonArray = [0, 1, 2, 3, 4, 5, 6, 7];
-    const buttons = buttonArray.map((beat, row) => (
+    const buttons = buttonCols.map((beat, row) => (
       <SampleButton
         key={beat + name}
         beat={beat}
@@ -86,51 +93,21 @@ class Sampler extends React.Component {
   }
 
   makeTableOfButtons() {
-    const buttonArray = ['Sound0', 'Sound1', 'Sound2', 'Sound3'];
-    const buttonColumns = buttonArray.map((name, column) => (
+    const buttonColumns = buttonRows.map((name, column) => (
       <div key={name}>{this.makeColumnOfButtons(column, name, soundUrls[column])}</div>
     ));
     return buttonColumns;
   }
 
-  onFinishedPlaying(i, j) {
-    // when the beat is advanced to OUR beat, we want to trigger a sound
-    // when the beat is advanced to the NEXT beat, we want to keep PLAYING
-    // when we stop playing, we wanna make a note of that so we don't loop on re-render
-    let newStatus = this.state.playStatus.map(row => row.slice());
-    newStatus[i][j] = 'STOPPED';
-    this.setState({playStatus: newStatus});
-  }
-
-  renderSamples() {
-    // return soundUrls.map((url, i) => (
-    //   <Sound key={url} url={url} playStatus={this.state.buttons[i][this.state.currentBeat]} />
-    // ));
-    const allButtons = this.state.buttons;
-    let self = this;
-    return allButtons.map((thisButton, i) =>
-      thisButton.map((beat, j) => (
-        <Sound
-          key={'row' + i + 'col' + j}
-          url={soundUrls[i]}
-          // this.state.buttons[i][j] && this.state.currentBeat === beat ? 'PLAYING' : 'STOPPED'
-          playStatus={this.state.playStatus[i][j]}
-          onFinishedPlaying={self.onFinishedPlaying(i, j)}
-        />
-      ))
-    );
-  }
-
   advanceBeat() {
     const currentBeat = (this.state.currentBeat + 1) % 8;
-    let playStatus = this.state.playStatus.map(row => row.slice());
     for (let i = 0; i < this.state.buttons.length; ++i) {
       const enabled = this.state.buttons[i][currentBeat];
       if (enabled) {
-        playStatus[i][currentBeat] = 'PLAYING';
+        soundManager.play(buttonRows[i]);
       }
     }
-    this.setState({currentBeat, playStatus});
+    this.setState({currentBeat});
   }
 
   render() {
@@ -140,12 +117,35 @@ class Sampler extends React.Component {
         <div>
           <ul>{this.makeTableOfButtons()}</ul>
         </div>
-        <div className="SoundPlayers">{this.renderSamples()}</div>
       </div>
     );
   }
 }
 
 // ========================================
-const rootComponent = ReactDOM.render(<Sampler />, document.getElementById('root'));
-setInterval(() => rootComponent.advanceBeat(), 250);
+soundManager.setup({
+  onready: () => {
+    // load all sounds before rendering
+    const promises = [];
+    for (const sound in soundFiles) {
+      const url = soundFiles[sound];
+      promises.push(
+        new Promise(function(resolve, reject) {
+          soundManager.createSound({
+            id: sound,
+            url: url,
+            autoLoad: true,
+            autoPlay: false,
+            onload: resolve,
+            volume: 100,
+          });
+        })
+      );
+    }
+
+    Promise.all(promises).then(() => {
+      const rootComponent = ReactDOM.render(<Sampler />, document.getElementById('root'));
+      setInterval(() => rootComponent.advanceBeat(), 1000);
+    });
+  },
+});
