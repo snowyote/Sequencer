4f4fe6752376d0bf19973030deb72ead424ef56e
lukemorse <luke.morse@gmail.com>
2017-11-22 13:21:35 -0600

    Add subdivision

--- src/index.js
+++ src/index.js
@@ -6,19 +6,21 @@ import 'react-rangeslider/lib/index.css';
 import {soundManager} from 'soundmanager2';
 
 // const buttonCols = [0, 1, 2, 3, 4, 5, 6, 7];
+var numBeats = 16;
+var subDiv = 4;
 var buttonCols = [];
-for (var i = 1; i <= 16; i++) {
+for (var i = 1; i <= numBeats; i++) {
   buttonCols.push(i);
 }
 
-const buttonRows = ['Sound0', 'Sound1', 'Sound2', 'Sound3', 'Sound4'];
-const soundFiles = ['kick 11.wav', 'Hat 27.wav', 'Hat 52.wav', 'Clap 13.wav', 'snare 347.wav'];
+const buttonRows = ['Sound0', 'Sound1', 'Sound2', 'Sound3'];
+const soundFiles = ['kick 11.wav', 'Hat 52.wav', 'Clap 13.wav', 'snare 347.wav'];
 
 class PlayPauseButton extends React.Component {
   constructor() {
     super();
     this.state = {
-      status: '>',
+      status: 'PLAY',
     };
   }
   onClick() {}
@@ -75,7 +77,21 @@ class Sampler extends React.Component {
     };
   }
 
+  getButtonClassName(active, num) {
+    var result = '';
+    var div = numBeats / subDiv;
+    var arr = [0, 1, 2, 3, 8, 9, 10, 11];
+    if (active) {
+      result += 'active';
+    }
+    if (arr.includes(num)) {
+      result += ' altBeat';
+    }
+    return result;
+  }
+
   makeColumnOfButtons(column, name) {
+    // className={this.getButtonClassName(this.state.buttons[column][row], row)}
     const buttons = buttonCols.map((beat, row) => (
       <SampleButton
         key={beat + name}
@@ -154,7 +170,7 @@ soundManager.setup({
 
     Promise.all(promises).then(() => {
       const rootComponent = ReactDOM.render(<Sampler />, document.getElementById('root'));
-      let interval = setInterval(() => rootComponent.advanceBeat(), 125);
+      let interval = setInterval(() => rootComponent.advanceBeat(), 200);
       // later, to cancel:
       // clearInterval(interval);
     });
